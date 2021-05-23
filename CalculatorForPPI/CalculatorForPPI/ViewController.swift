33a66ecfad251f2fb1a8baa6a4c2545be3b15db0
卢家涛 <lujiatao@lujiataodeMacBook-Air.local>
2021-05-23 13:06:27 +0800

    初始化

--- CalculatorForPPI/CalculatorForPPI/ViewController.swift
+++ CalculatorForPPI/CalculatorForPPI/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var button: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     @IBAction func calculate(_ sender: UIButton) {
         let width: Int = Int(textField1.text!)!
         let height: Int = Int(textField2.text!)!
@@ -27,11 +27,11 @@ class ViewController: UIViewController {
         let result: Int = Int(round(sqrt((pow(Double(width), 2) + pow(Double(height), 2)) / pow(size, 2))))
         label.text = "\(result) ppi"
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         textField1.resignFirstResponder()
         textField2.resignFirstResponder()
         textField3.resignFirstResponder()
     }
-    
+
 }
